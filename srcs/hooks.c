ffd5bd6b57e4ec0d58d9716a2ab7f7ed91e79f43
Daniela Sofia Do Carmo Padilha <ddo-carm@c2r1s11.42porto.com>
2025-02-09 18:59:38 +0000

    fixed esc key handling

--- srcs/hooks.c
+++ srcs/hooks.c
@@ -12,12 +12,16 @@
 
 #include "../include/fdf.h"
 
+//info --> uses the handle functions with hooks
+
 int	handle_events(t_fdf *fdf)
 {
-	mlx_hook(fdf->win, 2, 1L<<0, handle_keys, fdf);
-	mlx_hook(fdf->win, 17, 0L, close_window, fdf);
+	mlx_hook(fdf->win, 2, 1L << 0, handle_keys, fdf);
+	mlx_hook(fdf->win, 4, 1L << 2, handle_mouse, fdf);
+	mlx_hook(fdf->win, 17, 0L, clean_up, fdf);
 	return(0);
 }
+//info --> closes the window
 
 int	close_window(t_fdf *fdf)
 {
@@ -29,18 +33,47 @@ int	close_window(t_fdf *fdf)
 	exit(0);
 }
 
-int	handle_keys(t_fdf *fdf, int keycode)
+int		clean_up(t_fdf *fdf)
 {
+	mlx_destroy_image(fdf->mlx, fdf->img);
+    mlx_destroy_window(fdf->mlx, fdf->win);
+    mlx_destroy_display(fdf->mlx);
+    free_arrays((void **)fdf->map);
+    free(fdf->params);
+	exit(0);
+}
+
+//info --> handel key events
+
+int	handle_keys(int keycode, void *param)
+{
+	t_fdf *fdf;
+
+	fdf = (t_fdf *)param; 
+	printf("Key pressed: %d\n", keycode);
 	if(keycode == KEY_ESC)
-		close_window(fdf);
+		clean_up(fdf);
 	return (0);
 }
 
-int	handle_mouse(t_fdf *fdf, int mousecode)
+//info --> handles mouse events
+
+int	handle_mouse(t_fdf *fdf, int mousecode, int x, int y)
 {
-	if (mousecode == MOUSE_UP)
+	(void)x;
+	(void)y;
+	if (mousecode == SCROLL_UP)
+	{
 		fdf->scale *= ZOOM_FACTOR;
-	else if (mousecode == MOUSE_DOWN)
+		if (fdf->scale > MAX_SCALE)
+			fdf->scale = MAX_SCALE;
+	}
+	else if (mousecode == SCROLL_DOWN)
+	{
 		fdf->scale /= ZOOM_FACTOR;
+		if (fdf->scale < MIN_SCALE)
+			fdf->scale = MIN_SCALE;
+	}
+	draw_map(fdf, fdf->params); 
 	return (0);
 }
